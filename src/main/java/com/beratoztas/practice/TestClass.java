4d97ae99616784110134189de8c9da8ed8a1d51d
Berat <berat230@gmail.com>
2025-04-30 02:10:44 +0300

    Test commit for EPAM autocode setup

--- src/main/java/com/beratoztas/practice/TestClass.java
+++ src/main/java/com/beratoztas/practice/TestClass.java
@@ -3,7 +3,6 @@ package com.beratoztas.practice;
 public class TestClass {
 
 	public static void main(String[] args) {
-		Runnable r = () -> System.out.println("Hello from Lambda");
-        r.run();
+		System.out.println("This is a test commit");
 	}
 }
